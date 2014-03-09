df5ccfe29600b59884be7131f7ea644a2781ea16
Steef Gijsbers <steef_gijsbers@hotmail.com>
2014-03-09 23:29:41 +0100

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   has_secure_password
   
   before_save { self.email = email.downcase }
+  before_create :create_remember_token
   
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
@@ -15,4 +16,18 @@ class User < ActiveRecord::Base
   
   validates :password, length: { minimum: 6 }
   
+  
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+  
+  def User.hash(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+  
+  private
+  
+    def create_remember_token
+      self.remember_token = User.hash(User.new_remember_token)
+    end
 end
